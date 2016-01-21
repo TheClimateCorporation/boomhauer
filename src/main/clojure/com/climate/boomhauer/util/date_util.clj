a53c1814a4da1ba5a0ca6351a9f478b3d121ab7a
Eric Turcotte <eturcotte@climate.com>
2016-01-20 21:51:30 -0600

    Replaced AlexaDateUtil Java class with Clojure namespace date-util

--- src/main/clojure/com/climate/boomhauer/util/date_util.clj
+++ src/main/clojure/com/climate/boomhauer/util/date_util.clj
@@ -1,24 +1,59 @@
 (ns com.climate.boomhauer.util.date-util
   (:require [clojure.tools.logging :as log]
-            [clj-time.format :as format])
-  (:import [com.climate.boomhauer AlexaDateUtil]
-           [org.joda.time DateTime Interval Days]))
+            [clj-time.format :as cf]
+            [clj-time.core :as ct]
+            [clojure.string :as string])
+  (:import [org.joda.time DateTime Interval Days DateTimeZone]
+           [org.joda.time.format DateTimeFormatter]))
 
-(defn date->speech
+(def day-formatter ^DateTimeFormatter (cf/formatter "EEEE MMMM" (DateTimeZone/UTC)))
+(def year-month-day ^DateTimeFormatter (cf/formatter "yyyy-MM-dd" (DateTimeZone/UTC)))
+(def year-week ^DateTimeFormatter (cf/formatter "yyyy-'W'ww" (DateTimeZone/UTC)))
+
+(def days-of-month ["" "1st" "2nd" "3rd" "4th" "5th" "6th" "7th" "8th" "9th" "10th" "11th" "12th" "13th" "14th" "15th"
+                    "16th" "17th" "18th" "19th" "20th" "21st" "22nd" "23rd" "24th" "25th" "26th" "27th" "28th" "29th"
+                    "30th" "31st"])
+
+(defn parse-date
   [date-string]
-  (when-let [date-time (format/parse date-string)]
-    (let [date (.toDate date-time)
-          yesterday (Interval. (.withTimeAtStartOfDay (.minusDays (DateTime/now) 1)) Days/ONE)
-          today (Interval. (.withTimeAtStartOfDay (DateTime/now)) Days/ONE)
-          tomorrow (Interval. (.withTimeAtStartOfDay (.plusDays (DateTime/now) 1)) Days/ONE)
-          is-yesterday? (.contains yesterday date-time)
-          is-today? (.contains today date-time)
-          is-tomorrow? (.contains tomorrow date-time)]
-      (log/debugf "date [%s], is yesterday [%s], is today [%s], is tomorrow [%s]" date is-yesterday? is-today?
-                  is-tomorrow?)
+  (if (not (string/blank? date-string))
+    (if (.contains date-string "W")
+      (let [start-date (cf/parse year-week date-string)
+            end-date (ct/plus start-date (ct/weeks 1))]
+        {:start-date start-date
+         :end-date   end-date})
+      (let [date (cf/parse year-month-day date-string)]
+        {:start-date date
+         :end-date   date}))
+    (let [date (.withTime (.withZone (DateTime/now) (DateTimeZone/UTC)) 0 0 0 0)]
+      {:start-date date
+       :end-date   date})))
 
-      (cond
-        is-yesterday? "yesterday"
-        is-today? "today"
-        is-tomorrow? "tomorrow"
-        :else (str "on " (AlexaDateUtil/getFormattedDate date true))))))
+(defn format-date-time
+  [^DateTime date-time ^DateTime now]
+  (let [day-of-week-and-month (.print day-formatter date-time)
+        day-of-month (nth days-of-month (.getDayOfMonth date-time))
+        date-speech (str "on " day-of-week-and-month " " day-of-month)
+        year (.getYear date-time)]
+    (if (= (.getYear now) year)
+      date-speech
+      (str date-speech ", " year))))
+
+(defn date-time->speech
+  [^DateTime date-time]
+  (let [now (.withZone (DateTime/now) (DateTimeZone/UTC))
+        yesterday (Interval. (.withTimeAtStartOfDay (.minusDays now 1)) Days/ONE)
+        today (Interval. (.withTimeAtStartOfDay now) Days/ONE)
+        tomorrow (Interval. (.withTimeAtStartOfDay (.plusDays now 1)) Days/ONE)
+        is-yesterday? (.contains yesterday date-time)
+        is-today? (.contains today date-time)
+        is-tomorrow? (.contains tomorrow date-time)]
+    (log/debugf "date [%s], is yesterday [%s], is today [%s], is tomorrow [%s]" date-time is-yesterday? is-today?
+      is-tomorrow?)
+
+    (cond
+      (nil? date-time) nil
+      is-yesterday? "yesterday"
+      is-today? "today"
+      is-tomorrow? "tomorrow"
+      :else (format-date-time date-time now))))
--- src/test/clojure/com/climate/boomhauer/util/date_util_test.clj
+++ src/test/clojure/com/climate/boomhauer/util/date_util_test.clj
@@ -1,28 +1,36 @@
 (ns com.climate.boomhauer.util.date-util-test
   (:require [com.climate.boomhauer.util.date-util :as du])
   (:use [clojure.test])
-  (:import [org.joda.time DateTime]
+  (:import [org.joda.time DateTime DateTimeZone]
            [org.joda.time.format DateTimeFormat DateTimeFormatter]))
 
-(deftest date->speech
+(deftest parse-date
+  (testing "nil date string"
+    (let [today (.withTime (.withZone (DateTime/now) (DateTimeZone/UTC)) 0 0 0 0)]
+      (is (= {:start-date today :end-date today} (du/parse-date nil)))))
+  (testing "blank date string"
+    (let [today (.withTime (.withZone (DateTime/now) (DateTimeZone/UTC)) 0 0 0 0)]
+      (is (= {:start-date today :end-date today} (du/parse-date "")))))
+  (testing "valid date string"
+    (let [date-time (.withDate (.withTime (.withZone (DateTime.) (DateTimeZone/UTC)) 0 0 0 0) 2013 10 13)]
+      (is (= {:start-date date-time :end-date date-time} (du/parse-date "2013-10-13")))))
+  (testing "valid date string with week span"
+    (let [start-date-time (.withDate (.withTime (.withZone (DateTime.) (DateTimeZone/UTC)) 0 0 0 0) 2015 11 23)
+          end-date-time (.withDate (.withTime (.withZone (DateTime.) (DateTimeZone/UTC)) 0 0 0 0) 2015 11 30)]
+      (is (= {:start-date start-date-time :end-date end-date-time} (du/parse-date "2015-W48"))))))
+
+(deftest date-time->speech
   (testing "nil date input returns nil"
-    (is (nil? (du/date->speech nil))))
-  (testing "blank date input returns nil"
-    (is (nil? (du/date->speech ""))))
-  (testing "invalid date input returns nil"
-    (is (nil? (du/date->speech "bogus"))))
+    (is (nil? (du/date-time->speech nil))))
   (testing "yesterday's date returns 'yesterday'"
-    (let [yesterday (.minusDays (DateTime/now) 1)
-          yesterday-date-string (.toString yesterday)]
-      (is (= "yesterday" (du/date->speech yesterday-date-string)))))
+    (let [yesterday (.minusDays (DateTime/now) 1)]
+      (is (= "yesterday" (du/date-time->speech yesterday)))))
   (testing "today's date returns 'today'"
-    (let [today (DateTime/now)
-          today-date-string (.toString today)]
-      (is (= "today" (du/date->speech today-date-string)))))
+    (let [today (DateTime/now)]
+      (is (= "today" (du/date-time->speech today)))))
   (testing "tomorrow's date returns 'tomorrow'"
-    (let [tomorrow (.plusDays (DateTime/now) 1)
-          tomorrow-date-string (.toString tomorrow)]
-      (is (= "tomorrow" (du/date->speech tomorrow-date-string)))))
+    (let [tomorrow (.plusDays (DateTime/now) 1)]
+      (is (= "tomorrow" (du/date-time->speech tomorrow)))))
   (testing "other date in current year returns 'on day-of-week month day-of-month'"
     (let [now (DateTime/now)
           ; jan 1st is convenient but avoid issues in jan or dec by using apr
@@ -30,12 +38,10 @@
                                [4 "April"]
                                [1 "January"])
           other-day-in-current-year (.withDate (DateTime.) (.getYear now) month 1)
-          other-day-in-current-year-date-string (.toString other-day-in-current-year)
           day-of-week (.print ^DateTimeFormatter (DateTimeFormat/forPattern "EEEE") other-day-in-current-year)
           expected-speech (str "on " day-of-week " " month-name " 1st")]
-      (is (= expected-speech (du/date->speech other-day-in-current-year-date-string)))))
+      (is (= expected-speech (du/date-time->speech other-day-in-current-year)))))
   (testing "other date in different year returns 'on day-of-week month day-of-month year'"
     (let [other-day-in-different-year (.withDate (DateTime.) 1980 4 30)
-          other-day-in-current-year-date-string (.toString other-day-in-different-year)
-          expected-speech "on Wednesday April 30th 1980"]
-      (is (= expected-speech (du/date->speech other-day-in-current-year-date-string))))))
+          expected-speech "on Wednesday April 30th, 1980"]
+      (is (= expected-speech (du/date-time->speech other-day-in-different-year))))))
